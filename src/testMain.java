27b2aa101056584b96104e4e9ce8a77f52ac3df8
fresh_ji <jihangchn@163.com>
2017-11-15 19:29:47 +0800

    Update

---
+++ src/testMain.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jihang on 2017/11/9.
+ */
+
+class test {
+    public static void main(String[] args) throws Throwable {
+        dmGit mydmGit = new dmGit();
+        int pID = mydmGit.addProject("p5");
+        //int pID2 = mydmGit.addTask(pID, "t1");
+        //int tID = mydmGit.addTask(pID, "t1");
+        //tID = mydmGit.addTask(pID, "t2");
+    }
+}
